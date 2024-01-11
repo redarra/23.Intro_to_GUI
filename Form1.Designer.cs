e8c41a26058243fa6cc499332b94aaea5e232a6f
A252420 <A252420@LSBSA-GQV6HW3>
2024-01-11 12:26:08 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _23.Intro_to_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(53, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(232, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(464, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(53, 279);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(53, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Surname";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(53, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Year of birth";
+            label3.Click += label3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(232, 91);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(464, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(232, 145);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(118, 31);
+            textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(523, 367);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(53, 209);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 25);
+            label4.TabIndex = 14;
+            label4.Click += label4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button2;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+namespace _23.Intro_to_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime currentDateTime = DateTime.Now;
+
+            MessageBox.Show("You are " + (currentDateTime.Year - Int32.Parse(textBox3.Text)).ToString() + " years old!");
+            label4.Text = "Welcome " + textBox1.Text + " " + textBox2.Text;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
